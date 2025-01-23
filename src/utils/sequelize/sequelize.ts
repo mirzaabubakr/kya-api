3471999420dda3732d35d4d23c9620eb0850b246
Muhammad Abubakr <62260960+mirzaabubakr@users.noreply.github.com>
2025-01-23 17:12:37 +0400

    completed

--- src/utils/sequelize/sequelize.ts
+++ src/utils/sequelize/sequelize.ts
@@ -1,6 +1,7 @@
 const { Sequelize } = require("sequelize");
 import dbConfig from "../../config/db";
 import { UserModel } from "../../modules/auth/models/user.model";
+import { register } from "../../modules/auth/service";
 import { KYCModel } from "../../modules/kyc/model/kyc.model";
 
 const env: string = process.env.NODE_ENV || "development";
@@ -20,5 +21,25 @@ const KYC = KYCModel(sequelize);
 
 User.hasOne(KYC, { foreignKey: "userId", as: "kyc" });
 KYC.belongsTo(User, { foreignKey: "userId", as: "user" });
+
+async function createDefaultAdmin() {
+  const adminData: any = {
+    name: "Admin",
+    email: "admin@admin.com",
+    password: "Admin!123",
+    role: "admin",
+  };
+  await register(adminData);
+}
+
+sequelize
+  .sync()
+  .then(async (): Promise<void> => {
+    console.log("Database connected successfully.");
+    await createDefaultAdmin();
+  })
+  .catch((err: Error): void => {
+    console.error("Error connecting to the database:", err);
+  });
 
 export { sequelize, User, KYC };
